df62b3c755bacf1b5bd17e6db75e90b1432646c3
Ilya Komolkin <komolkin@gmail.com>
2025-11-13 23:57:07 +0100

    Remove custom webpack configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   images: {
     domains: ['gateway.pinata.cloud', 'supabase.co', 'thumbnailer.livepeer.studio', 'playback.livepeer.com'],
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimize = false
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
